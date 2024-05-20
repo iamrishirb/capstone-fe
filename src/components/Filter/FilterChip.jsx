d2ebd68b29c0e6045f278e21cb2e9b3b50e49424
Rishi Bajpai <106062410+rishi-novo@users.noreply.github.com>
2024-05-20 11:50:48 +0530

    filter chips showing good

--- src/components/Filter/FilterChip.jsx
+++ src/components/Filter/FilterChip.jsx
@@ -3,13 +3,26 @@
 import React from 'react';
 import styles from './filters.module.css';
 
+const getRandomLightColor = () => {
+    const letters = 'BCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+};
+
 const FilterChip = ({ filter, onRemove }) => {
     const handleRemove = () => {
         onRemove(filter);
     };
 
+    const chipStyle = {
+        backgroundColor: getRandomLightColor(),
+    };
+
     return (
-        <div className={styles['filter-chip']}>
+        <div className={styles['filter-chip']} style={chipStyle}>
             <span>{filter}</span>
             <button className={styles["remove-btn"]} onClick={handleRemove}>
                 &times;
